7db2e108dd143aa7b0db2ce1f94e8e747352217e
Gabriel Barreto <syn.ga91@gmail.com>
2021-04-26 20:32:35 -0300

    Criação de Genserver para lidar com o envio de email e explicação sobre Supervisor

--- lib/inmana/application.ex
+++ lib/inmana/application.ex
@@ -14,14 +14,23 @@ defmodule Inmana.Application do
       # Start the PubSub system
       {Phoenix.PubSub, name: Inmana.PubSub},
       # Start the Endpoint (http/https)
-      InmanaWeb.Endpoint
+      InmanaWeb.Endpoint,
       # Start a worker by calling: Inmana.Worker.start_link(arg)
       # {Inmana.Worker, arg}
+
+      # Passo o meu Scheduler para ser executado assim que o app iniciar; É executado pelo Supervisor
+      Inmana.Supplies.Scheduler
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
+
+    # estategia de one_for_one, caso der um erro, der um problema, ele vai ver que o processo filho morreu
+    # e vai reabrir esse processo em outra thread; isso sem ferrar com os outros processos; É das opções
+    # passadas para o Supervisor
     opts = [strategy: :one_for_one, name: Inmana.Supervisor]
+    # Supervisor é um processo especial que supervisiona outros processos, ele chama automaticamente a
+    # função start_link presente em todos os childrens passados na lista acima.
     Supervisor.start_link(children, opts)
   end
 
