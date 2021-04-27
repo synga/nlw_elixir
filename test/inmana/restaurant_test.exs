5188f4a5e49d04a700fa3c5ac745b8f2559f752d
Gabriel Barreto <syn.ga91@gmail.com>
2021-04-27 14:24:07 -0300

    Merge de aula 4 e removidos comentarios adicionais

--- test/inmana/restaurant_test.exs
+++ test/inmana/restaurant_test.exs
@@ -1,7 +1,4 @@
 defmodule Inmana.RestaurantTest do
-  # Aqui preciso usar o Inmana.DataCase pois ele tem tudo que precisamos para teste de changeset
-  # Falo mais nas anotações
-  # ,async: true executa todos os testes asyncronamente, PORÉM usar só com Postgre. Mais nas anotações
   use Inmana.DataCase, async: true
 
   alias Ecto.Changeset
@@ -13,35 +10,19 @@ defmodule Inmana.RestaurantTest do
 
       response = Restaurant.changeset(params)
 
-      # Aqui vou fazer o teste pegando por pattern match e dando um assert nisso;
-      # Primeiro fez falhar para ver qual era o resultado; viu que o resultado era um changeset com os
-      # valores abaixo; então criou um alias de Changeset e criou um changeset igual ao retorno dado
-      # pela função no response, visto que é um retorno valido; Depois, por pattern match, como ele retorna
-      # um true se estiver tudo certo, vou dar um assert que os dados do changeset são iguais ao response
       assert %Changeset{changes: %{email: "siri@cascudo.com", name: "Siri Cascudo"}, valid?: true} = response
     end
 
     # Testando erros
     test "When there are invalid params, returns an invalid changeset" do
       params = %{name: "S", email: "siricascudocom"}
-      # Crio uma resposta que espero do erro, então baseado no params acima e nas validações existentes
-      # Esses são os erros que espero receber no response
+
       expected_response = %{email: ["has invalid format"], name: ["should be at least 2 character(s)"]}
 
-      # Chamo a função com meu params para pegar o resultado
       response = Restaurant.changeset(params)
 
-      # aqui só o %Changeset{valid?: false} = response já funciona para verificar se o retorno é valido
-      # ou não, o que esperamos que não seja, por isso verifico apenas se o changeset retornado tem um
-      # valid setado em false
       assert %Changeset{valid?: false} = response
 
-      # Aqui estou verificando usando a função errors_on que está disponivel no DataCase - que é basicamente
-      # a função de traverse_errors que foi criada no meu arquivo de errors - se os errors retornados no
-      # meu response são os mesmos que tenho no expected_response, e aqui como é só uma lista, ai sim
-      # uso a comparação de igualdade e não pattern match, tentei chamar como pattern match para teste
-      # e ele falhou;
-      # Então a errors_on vai pegar do meu changeset retornado apenas os errors
       assert errors_on(response) == expected_response
     end
   end
--- test/inmana_web/views/restaurants_view_test.exs
+++ test/inmana_web/views/restaurants_view_test.exs
@@ -1,37 +1,18 @@
-# Aqui ele não passa pelo controller pra fazer o derive e pegar só o que precisa, o meu create_restaurant
-# vai me retornar TUDO da criação de um restaurante
 defmodule InmanaWeb.RestaurantsViewTest do
-  # Para teste de view também uso o ConnCase
-  # ,async: true executa todos os testes asyncronamente, PORÉM usar só com Postgre. Mais nas anotações
   use InmanaWeb.ConnCase, async: true
 
-  # Preciso importar o Phoenix.View para expor o metodo render/3()
   import Phoenix.View
 
-  # Crio um alias do Inmana.RestaurantView pois o render aqui não sabe de qual modulo o "create.json" pertence
   alias Inmana.Restaurant
   alias InmanaWeb.RestaurantsView
 
-  # Quero testar o render com aridade 2
   describe "render/2" do
-    # Só tenho 1 teste aqui, que é o de sucesso ao criar a view
     test "renders create.json" do
-      # As params para criação do meu restaurante
       params = %{name: "Siri Cascudo", email: "siri@cascudo.com"}
-      # Crio um restaurante e já por pattern match pego o status de :ok e o restaurante devolvido
       {:ok, restaurant} = Inmana.create_restaurant(params)
-      # Crio minha view guardando ela em response; Passo o ResturantsView pois aqui não tem aquela ligação
-      # Em que o view sabe a qual controller ele pertence; Depois passo o nome da view a ser criada; e
-      # por ultimo passo os dados do restaurante; Esse render é igual ao Render no RestaurantsView
+
       response = render(RestaurantsView, "create.json", restaurant: restaurant)
 
-      # Por pattern match faço o assert de que essa lista que tenho deve ser igual ao response. porém
-      # Removo dados como __meta__ e supplies que não vão ser utilizados, e created_at e updated_at que
-      # são dados que mudam a cada criação; O id quero verificar se ele existe, porém o valor do id não
-      # importa, então deixo como inutilizado;
-      # !!!Pesquisar!!! ao colocar essa lista em uma variavel me deu um erro falando que _id/0 não estava
-      # definido; De alguma forma ele achou que esse _id era uma função. Por que acontece isso fora do
-      # Pattern Match? É a forma correta?
       assert %{
         message: "Restaurant created!",
         restaurant: %Restaurant{
