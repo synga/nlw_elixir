6dad0d10414dc7f28c2d6184daac755dace625c1
Gabriel Barreto <gabriel-gdr@hotmail.com>
2021-08-29 16:29:14 -0300

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :inmana, Inmana.Repo,
   username: "postgres",
-  password: "qZ1wX2dev3&",
+  password: "postgres",
   database: "inmana_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
