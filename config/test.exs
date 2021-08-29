ad9392333960e85824c13314b9e07765df9f08b5
Gabriel Barreto <gabriel-gdr@hotmail.com>
2021-08-29 16:29:37 -0300

    Update test.exs

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :inmana, Inmana.Repo,
   username: "postgres",
-  password: "qZ1wX2dev3&",
+  password: "postgres",
   database: "inmana_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
